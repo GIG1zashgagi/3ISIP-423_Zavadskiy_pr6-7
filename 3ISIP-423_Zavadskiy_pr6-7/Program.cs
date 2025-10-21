71bad0f0a67d9eea7e4868fdd4d18d42de7aed0f
Илья Завадский <ilazavadskij715@gmail.com>
2025-10-21 11:26:39 +0300

    создаем абстрактный класс и вносим в него данные

--- 3ISIP-423_Zavadskiy_pr6-7/Program.cs
+++ 3ISIP-423_Zavadskiy_pr6-7/Program.cs
@@ -1,2 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+
+namespace RogueLikeGame
+{
+    public abstract class Item      // абстрактый класс без аналогов
+    {
+        public string Name { get; protected set; }      // 'get' - можно свободно прочитать; 'protected set' - установка значения доступна только внутри класса и производных классов
+        public int Attack {  get; protected set; }
+        public int Defence { get; protected set; }
+
+        protected Item(string name, int attack = 0, int defence = 0)
+        {
+            Name = name;
+            Attack = attack;
+            Defence = defence;
+        }
+    }
+
+    public class Weapon : Item      // создаем класс 'Weapon' который связан с абстрактным классом 'Item'
+    {
+        public Weapon (string name, int attack) : base (name, attack, 0) { }
+    }
+
+    public class Armor : Item     // создаемм класс 'Armor' который связан с абстрактным классом 'Item'
+    {
+        public Armor (string name, int defence) : base (name, defence, 0) { }
+    }
+
+
+}

8e172f543ed49ce5f63d2eebcb787c4784156986
Илья Завадский <ilazavadskij715@gmail.com>
2025-10-27 11:30:12 +0300

    создаем врага гоблина со встроеной в нее базой данных enemy

--- 3ISIP-423_Zavadskiy_pr6-7/Program.cs
+++ 3ISIP-423_Zavadskiy_pr6-7/Program.cs
@@ -95,4 +95,34 @@ namespace TextRoguelike     // создаем пространство для н
             return $"{Name} - HP: {HP}/{MaxHP}, Атака: {Attack}, Защита: {Defense}";
         }
     }
+    public class Goblin : Enemy
+    {
+        private double critChance = 0.2;
+
+        public Goblin(Random random)
+        {
+            Name = "Гоблин";
+            Type = EnemyType.Goblin;
+            MaxHP = 30 + random.Next(-5, 6);
+            HP = MaxHP;
+            Attack = 8 + random.Next(-2, 3);
+            Defense = 3 + random.Next(-1, 2);
+        }
+
+        public override bool TrySpecialAbility(Player player, Random random)
+        {
+            return false; 
+        }
+
+        public override int CalculateDamage(Player player, Random random)
+        {
+            int baseDamage = Attack;
+            if (random.NextDouble() < critChance)
+            {
+                Console.WriteLine("Критический удар гоблина!");
+                baseDamage = (int)(baseDamage * 1.5);
+            }
+            return Math.Max(1, baseDamage - player.GetDefense());
+        }
+    }
 }

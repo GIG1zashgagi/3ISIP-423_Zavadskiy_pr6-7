e1808122ee568307b5ce46d0670a31a53608ae8d
Илья Завадский <ilazavadskij715@gmail.com>
2025-10-27 11:15:18 +0300

    создаем пространство для нашей программы

--- 3ISIP-423_Zavadskiy_pr6-7/Program.cs
+++ 3ISIP-423_Zavadskiy_pr6-7/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
+namespace TextRoguelike     // создаем пространство для нашей программы
+{
+    class Program       // создаем класс 'Program'
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
